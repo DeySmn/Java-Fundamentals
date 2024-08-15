7bc303728b38772bbc59769bbe6e3a0ecbab951e
Sumon Dey <deysumon99@gmail.com>
2024-08-15 23:52:06 +0530

    added some more additional topics

--- JavaFundamentals-decisionControlStructures/src/main/Example4.java
+++ JavaFundamentals-decisionControlStructures/src/main/Example4.java
@@ -13,6 +13,11 @@ public class Example4 {
             else
                 System.out.println("B");
 
+        /*
+        O/p:
+        Nothing will be printed, as parent if block condition fails, and
+        we don't have any else block is associated with parent if block.
+         */
 
     }
 }
